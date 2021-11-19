f23b91ba0d3ec35bbf44641c7ec30cc4e69e61cc
JaviCasti <castillejo.corral@gmail.com>
2021-11-19 04:21:34 +0100

    Estructuras de Control en Python

---
+++ Python/EstructurasControl.py
@@ -0,0 +1,40 @@
+    #If
+#Basico
+if 1==1:
+    pass
+#Elseif
+if 1==1:
+    pass
+else:
+    pass
+#If corto
+print("Si") if 1==1 else print("No")
+    
+    #While
+while 1==1:
+    pass
+
+#Hacer algo al final el bucle
+while 1 ==1:
+    print()
+else:
+    print()
+    
+    #For
+lista = []
+for x in lista:
+    pass
+#Recorrer String
+for x in "banana":
+    pass
+
+#Evitar elementos
+for x in lista:
+    if x == "Hola":
+        continue
+    pass
+
+#For 'normal', en un rango de numeros
+
+for x in range(6):
+    pass
